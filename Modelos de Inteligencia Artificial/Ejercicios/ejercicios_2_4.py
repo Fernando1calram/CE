046626517c96a4179431a2fde826f8da2fad0273
Fernando1calram <130754557+Fernando1calram@users.noreply.github.com>
2026-09-24 19:44:38 +0200

    (R) Clase 24/09/2026

---
+++ Modelos de Inteligencia Artificial/Ejercicios/ejercicios_2_4.py
@@ -0,0 +1,55 @@
+""" 
+-> Turismo
+1. Qué problemas resuelve?
+    - Creación de itinerarios para viajes.
+    - Filtrado de alojamiento atendiendo a unas características específicas
+
+2. Qué tecnología de IA usa?
+    - Sistemas de recomendación
+    - Aprendizaje profundo
+
+3. Qué beneficio aporta?
+    - Viajes más personalizados
+    - Ahorro de tiempo y dinero
+    - Mayor accesibilidad
+
+4. Cuáles son sus limitaciones actuales?
+    - Necesidad de datos correctos y actualizados
+    - Inventar horarios, rutas o servicios
+
+    
+-> Traducción
+1. Qué problemas resuelve?
+    - Traducción automática de documentos
+    - Traducción de conversaciones en tiempo real
+
+2. Qué tecnología de IA usa?
+    - La comisión europea usa eTranslation, eSummary
+
+3. Qué beneficio aporta?
+    - Elimina barreras linguisticas
+    - Procesa rápidamente grandes volúmenes de texto
+
+4. Cuáles son sus limitaciones actuales?
+    - Errores tipográficos en la traducción
+    - Fallos en la simultaneidad de la traducción o 
+    retrasos por el corto tiempo de espera que precisa
+
+    
+-> Bibliotecas
+1. Qué problemas resuelve?
+    - Catalogación lenta
+    - Fondos mal descritos
+
+2. Qué tecnología de IA usa?
+    - OCR (para convertir imágenes a texto)
+    - Machine learning (clasifica documentos)
+    - Deep learning (reconoce escritura manuscrita)
+
+3. Qué beneficio aporta?
+    - Catalogación prácticamente automáticas de grandes volúmenes de documentos
+
+4. Cuáles son sus limitaciones actuales?
+    - Fallos al interpretar manuscritos
+    - Que cuente con datos de mala calidad
+"""
